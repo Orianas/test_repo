d2c93293f38a9ea86c7b3966d1a8203a0c3d178f
Steve Gilland <steven.gilland@gmail.com>
2022-02-02 08:40:18 -0600

    * Added .gitignore * Color Picker Dialog

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string>
 
 int main () {
-    std::cout << "Hello, World!" << std::endl;
+    std::string color;
+    
+    std::cout << "What is your favorite color? ";
+    std::cin >> color;
+
+    std::cout << "That's great that your favorite color is " << color << "." << std::endl;
 
     return 0;
 }
